ca1c261477b9a3445cd95fea77c048c502c1a346
Semen Teplov <eburg-severca@mail.ru>
2023-05-02 22:45:05 +0500

    Add project

---
+++ DZ_Thread/DZ_Thread/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+
+    static Thread chickenVoice;
+    static Thread eggVoice;
+    static int count = 0;
+    static void Main(string[] args)
+    {
+        chickenVoice = new Thread(new ThreadStart(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(new Random().Next(100, 500));
+                Console.WriteLine(count++ + ") Chicken # " + i);
+            }
+            if (!eggVoice.IsAlive) Console.WriteLine("Первым появилось курица");
+        }));
+
+        eggVoice = new Thread(new ThreadStart(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(new Random().Next(100, 500));
+                Console.WriteLine(count++ + ") Egg! # " + i);
+            }
+            if (!chickenVoice.IsAlive) Console.WriteLine("Первым появилось яйцо");
+        }));
+
+        eggVoice.Start();
+        chickenVoice.Start();
+
+    }
+}
